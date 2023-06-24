9d52da1bbf068cff1786c5d1f829ba01c689f625
William Wang <bn.william.w@gmail.com>
2023-06-24 13:25:12 -0400

    Add text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,43 @@
 import { CredentialType, IDKitWidget } from "@worldcoin/idkit";
 import type { ISuccessResult } from "@worldcoin/idkit";
 import styles from "../styles/Home.module.css";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Home() {
+  const [verified, setVerified] = useState(false);
 
-	const [verified, setVerified] = useState(false);
+  const onSuccess = (result: ISuccessResult) => {
+    // This is where you should perform frontend actions once a user has been verified, such as redirecting to a new page
+    setVerified(true);
+  };
 
-	const onSuccess = (result: ISuccessResult) => {
+  const handleProof = async (result: ISuccessResult) => {
+    const reqBody = {
+      merkle_root: result.merkle_root,
+      nullifier_hash: result.nullifier_hash,
+      proof: result.proof,
+      credential_type: result.credential_type,
+      action: process.env.NEXT_PUBLIC_WLD_ACTION_NAME,
+      signal: "",
+    };
+    fetch("/api/verify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqBody),
+    }).then(async (res: Response) => {
+      if (res.status == 200) {
+        console.log("Successfully verified credential.");
+      } else {
+        throw (
+          new Error("Error: " + (await res.json()).code) ?? "Unknown error."
+        );
+      }
+    });
+  };
 
-		// This is where you should perform frontend actions once a user has been verified, such as redirecting to a new page
-		setVerified(true);
-	};
-
-	const handleProof = async (result: ISuccessResult) => {
-		const reqBody = {
-			merkle_root: result.merkle_root,
-			nullifier_hash: result.nullifier_hash,
-			proof: result.proof,
-			credential_type: result.credential_type,
-			action: process.env.NEXT_PUBLIC_WLD_ACTION_NAME,
-			signal: "",
-		};
-		fetch("/api/verify", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(reqBody),
-		}).then(async (res: Response) => {
-			if (res.status == 200) {
-				console.log("Successfully verified credential.")
-			} else {
-				throw new Error("Error: " + (await res.json()).code) ?? "Unknown error.";
-			}
-		});
-	};
-
-	const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -62,42 +62,76 @@ export default function Home() {
     transition: "background 0.3s",
   };
 
-	return (
-		<div className={styles.container}>
-			{!verified &&
-			<div style={{ display: "flex", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
-				<IDKitWidget action={process.env.NEXT_PUBLIC_WLD_ACTION_NAME!} onSuccess={onSuccess} handleVerify={handleProof} app_id={process.env.NEXT_PUBLIC_WLD_APP_ID!} credential_types={[CredentialType.Orb, CredentialType.Phone]}>
-					{({ open }) => <button
-          onClick={open}
+  return (
+    <div className={styles.container}>
+      {!verified && (
+        <div
           style={{
-            padding: "10px 20px",
-            fontSize: "16px",
-            fontWeight: "bold",
-            borderRadius: "5px",
-            color: "#fff",
-            backgroundColor: "#007bff",
-            border: "none",
-            cursor: "pointer",
-            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-            transition: "background-color 0.3s",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            minHeight: "100vh",
           }}
-          onMouseEnter={(e) => {
-            e.target.style.backgroundColor = "#0056b3";
-          }}
-          onMouseLeave={(e) => {
-            e.target.style.backgroundColor = "#007bff";
-          }}
-          onMouseDown={(e) => {
-            e.target.style.backgroundColor = "#003d7b";
-          }}
-          onMouseUp={(e) => {
-            e.target.style.backgroundColor = "#0056b3";
-          }}
-        >Verify with World ID</button>}
-				</IDKitWidget>
-			</div> }
-			{verified && <iframe src="https://creator.voiceflow.com/prototype/64964832cba8be000738c63e" width="100%" height="600"></iframe>
-}
-		</div>
-	);
+        >
+          <div>
+            <title>Mint your very own ETHWaterloo POAP using Goose.ai!</title>
+            <h2>
+              Meet Goose.ai, the cyber companion that will steal your heart with
+              every honk. This delightful goose is here to revolutionize your
+              digital journey with cuddles and unwavering loyalty. He's not your
+              average sidekick – he's a feathered friend that will keep you safe
+              and snug in the ever-changing cyber realm.
+            </h2>
+          </div>
+          <IDKitWidget
+            action={process.env.NEXT_PUBLIC_WLD_ACTION_NAME!}
+            onSuccess={onSuccess}
+            handleVerify={handleProof}
+            app_id={process.env.NEXT_PUBLIC_WLD_APP_ID!}
+            credential_types={[CredentialType.Orb, CredentialType.Phone]}
+          >
+            {({ open }) => (
+              <button
+                onClick={open}
+                style={{
+                  padding: "10px 20px",
+                  fontSize: "16px",
+                  fontWeight: "bold",
+                  borderRadius: "5px",
+                  color: "#fff",
+                  backgroundColor: "#007bff",
+                  border: "none",
+                  cursor: "pointer",
+                  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+                  transition: "background-color 0.3s",
+                }}
+                onMouseEnter={(e) => {
+                  e.target.style.backgroundColor = "#0056b3";
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.backgroundColor = "#007bff";
+                }}
+                onMouseDown={(e) => {
+                  e.target.style.backgroundColor = "#003d7b";
+                }}
+                onMouseUp={(e) => {
+                  e.target.style.backgroundColor = "#0056b3";
+                }}
+              >
+                Verify with World ID
+              </button>
+            )}
+          </IDKitWidget>
+        </div>
+      )}
+      {verified && (
+        <iframe
+          src="https://creator.voiceflow.com/prototype/64964832cba8be000738c63e"
+          width="100%"
+          height="100%"
+        ></iframe>
+      )}
+    </div>
+  );
 }
